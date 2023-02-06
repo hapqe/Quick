77f79aed4e090eca900cf641d65b6a18f0874b6c
Konrad Hapke <hap19520@spengergasse.at>
2023-02-06 21:59:51 +0100

    delete stuff

--- Assets/Editor/BlenderTools/Shortcuts.cs
+++ Assets/Editor/BlenderTools/Shortcuts.cs
@@ -68,44 +68,4 @@ public static class Shortcuts
         }
         Collapse();
     }
-
-    [MenuItem("Edit/Copy Materials %L")]
-    static void CopyMaterials()
-    {
-        var sel = Selection.gameObjects;
-        if(sel.Length == 0) return;
-
-        var active = Selection.activeGameObject;
-
-        var activeMat = active.GetComponent<Renderer>()?.sharedMaterial;
-
-        if(activeMat == null) return;
-
-        foreach(var go in sel) {
-            var rend = go.GetComponent<Renderer>();
-            if(rend == null) continue;
-
-            Undo.RecordObject(rend, "Set Material");
-
-            rend.sharedMaterial = activeMat;
-        }
-
-        Collapse();
-    }
-
-    static void Spawn(int number) {
-        EditorApplication.ExecuteMenuItem("Edit/Selection/Load Selection " + number);
-        if(Selection.gameObjects.Length == 0) return;
-        var p = PrefabUtility.InstantiatePrefab(Selection.gameObjects[0]);
-        if(p == null) return;
-
-        if(p as GameObject) {
-            (p as GameObject).transform.position = Cursor.position;
-        }
-        Undo.RegisterCreatedObjectUndo(p, "Create Prefab");
-
-        Selection.activeGameObject = null;
-        if(p as GameObject != null)
-        Selection.activeGameObject = p as GameObject;
-    }
 }
